97bcf867494b40c434bb59edfdc2d0d54e7f6d37
ehteesham <ehteshaam44>
2023-10-11 23:11:54 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '[%(asctime)s]: %(message)s:')
+
+project_name = 'CNNClassifier'  # we can re-name according to our project
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)  # Path will convert the file address as a window file path
+    file_dir, file_name = os.path.split(filepath)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating Directory: {file_dir} for the file: {file_name}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{file_name} is already exists")
